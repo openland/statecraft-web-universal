1a968d3e7a43ca569fcc638275307739425d6497
Steve Kite <korshakov.stepan@gmail.com>
2017-12-09 05:08:42 +0300

    Adding redirect to https

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const compression = require('compression');
 const express = require('express')
 const next = require('next')
+var redirectToHTTPS = require('express-http-to-https').redirectToHTTPS
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
@@ -11,6 +12,9 @@ app.prepare()
     .then(() => {
         const server = express()
         server.use(compression());
+        if (process.env.NODE_ENV === 'production') {
+            server.use(redirectToHTTPS());
+        }
 
         server.get('*', (req, res) => {
             return handle(req, res)
